64cae52c22cd25e0f339221426d1505d1ed5c6a2
Jeffrey <jeffre.concha@mobileoptima.com>
2017-05-24 20:12:08 +0800

    Tarkie Attendance first commit.

--- app/src/main/java/com/mobileoptima/core/Rx.java
+++ app/src/main/java/com/mobileoptima/core/Rx.java
@@ -1,5 +1,7 @@
 package com.mobileoptima.core;
 
+import android.util.Log;
+
 import com.codepan.callback.Interface.OnErrorCallback;
 import com.codepan.database.Condition;
 import com.codepan.database.FieldValue;
