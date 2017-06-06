74059336d828abf97e4b153ff8695fbb28e9e039
Jeffrey <jeffre.concha@mobileoptima.com>
2017-06-06 14:41:54 +0800

    Mergin of paul and master

--- app/src/main/java/com/mobileoptima/tarkieattendance/SearchEntriesFragment.java
+++ app/src/main/java/com/mobileoptima/tarkieattendance/SearchEntriesFragment.java
@@ -120,7 +120,8 @@ public class SearchEntriesFragment extends Fragment implements OnClickListener,
 			}
 		});
 		stlSearchEntries.setCustomTabView(R.layout.tab_item_layout, R.id.tvTab);
-		stlSearchEntries.setDistributeEvenly(isTablet);
+		//stlSearchEntries.setDistributeEvenly(isTablet);
+		stlSearchEntries.setDistributeEvenly(false);
 		stlSearchEntries.setViewPager(vpSearchEntries);
 		stlSearchEntries.setSelectedTypeface(bold);
 		stlSearchEntries.setSelectedColor(gray);
